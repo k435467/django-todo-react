1633eb045d4213cbc7f57e93683f57b13f1462f7
Ethan Kohl <k435467@gmail.com>
2021-01-23 14:58:24 +0800

    docker-compose

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'dtd4c&!v-yaummb7pt9z)tw@nfy3xv#m57d1-mep1=&^empu(n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['backend']
 
 
 # Application definition
